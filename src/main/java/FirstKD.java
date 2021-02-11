003ed568433cf00102e35f05f1f3818b48849f0c
DimanKom <komarov198@gmail.com>
2021-02-11 18:21:31 +0300

    first commit

---
+++ src/main/java/FirstKD.java
@@ -0,0 +1,12 @@
+// Первый комментарий. Тренировка.
+/* Блок комментов
+dfdf
+dfdf
+dfdf
+*/
+public class FirstKD {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Что то еще пишу");
+    }
+}
